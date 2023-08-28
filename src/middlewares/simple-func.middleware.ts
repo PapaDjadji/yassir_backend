8e3ab23c4572a735dcc69f54ec883f15b123df5f
papa_djadji <djadjiapporio@gmail.com>
2023-08-28 17:55:11 +0000

    pollution info

--- src/middlewares/simple-func.middleware.ts
+++ src/middlewares/simple-func.middleware.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from 'express'
 
 export function simpleFunc(req: Request, res: Response, next: NextFunction) {
-  console.log('Executing request after the function middleware...');
   next();
 } 
